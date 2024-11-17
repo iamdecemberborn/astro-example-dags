5e6e5f5f0d4f365a34f1ec3eb34a2f0644f25480
JungleIsBeautiful <40429093+iamdecemberborn@users.noreply.github.com>
2024-11-18 04:53:55 +0530

    added in function

--- dags/0011.py
+++ dags/0011.py
@@ -93,6 +93,32 @@ def create_and_read_csv111():
         print("Contents of Current Path:", contents)
 
 
+        # Get the current working directory
+        current_path = os.getcwd()
+        print("Current Path:", current_path)
+
+        # Navigate to the parent folder
+        parent_path = os.path.dirname(current_path)
+        print("Parent Path:", parent_path)
+
+        # Navigate to the grandparent folder
+        grandparent_path = os.path.dirname(parent_path)
+        print("Grandparent Path:", grandparent_path)
+
+        # List all directories and files in the current path
+        contents_current = os.listdir(current_path)
+        print("Contents of Current Path:", contents_current)
+
+        # List all directories and files in the parent path
+        contents_parent = os.listdir(parent_path) if os.path.exists(parent_path) else []
+        print("Contents of Parent Path:", contents_parent)
+
+        # List all directories and files in the grandparent path
+        contents_grandparent = os.listdir(grandparent_path) if os.path.exists(grandparent_path) else []
+        print("Contents of Grandparent Path:", contents_grandparent)
+
+
+
 
 
 
